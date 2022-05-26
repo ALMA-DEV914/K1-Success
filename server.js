dc3755237d97e583ecea5134708c1a42cf74783c
ALMA_DEV914 <aungonalna58@gamil.com>
2022-05-26 12:42:04 -0700

    add get routes for id parameter

--- server.js
+++ server.js
@@ -43,13 +43,28 @@ function filterByQuery(query, storiesArray ){
     return filteredResults;
 }
 
+function findById(id, storiesArray){
+    const result = storiesArray.filter(story => story.id === id[0]);
+    return result;
+}
+
 app.get('/api/stories', (req, res) => {
     let results = stories;
     if(req.query){
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/stories/:id', (req, res) => {
+    const result = findById(req.params.id, stories);
+    if(result){
+    res.json(result);
+    } else {
+        res.send(404)
+    }
 })
+
 
 app.listen(PORT, () => {
     console.log(`API server now on ${PORT}`)
